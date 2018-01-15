72f16669ddcc2f71b99e8bc15913e83599f008ec
quangld <dangquanglebk@gmail.com>
2018-01-15 13:28:54 +0700

    recovery cars command

--- app/Console/Commands/recoverCars.php
+++ app/Console/Commands/recoverCars.php
@@ -37,12 +37,38 @@ class recoverCars extends Command
      */
     public function handle()
     {
-        $address = base_path('assets/chassisNo.xlsx');
-        \Excel::load($address, function ($reader) {
-            foreach($reader->toArray() as $row)
-            {
-            print_r($row);
+        ///export?format=xlsx&gid
+        $dailyCheck = base_path('assets/dailyCheck.xlsx');
+        $chassisNo = base_path('assets/chassisNo.xlsx');
+        $exportFile = fopen(base_path('assets/recover_deleted_cars_'.date('Ymd').'.sql'), 'w+');
+        $data = \Excel::load($dailyCheck, function ($reader) {
+        }, 'UTF-8')->get();
+
+        $chassis = \Excel::load($chassisNo, function ($reader) {
+        }, 'UTF-8')->get();
+
+        if(empty($chassis) || !$chassis->count()){
+            echo "Error: ChassisNo file is empty";
+            return;
+        }
+
+        if(empty($data) || !$data->count()){
+            echo "Error: DailyCheck File is empty";
+            return;
+        }
+
+        $total = 0;
+        foreach ($chassis as $key => $value1) {
+            foreach ($data as $key => $value2) {
+                if((string)$value1->車台番号 === (string)$value2->車台番号) {
+                    $total++;
+                    fwrite($exportFile, "UPDATE shop_cars SET delete_flg=0 WHERE id='$value2->id';\n");
+                    var_dump($value2->id);
+                };
             }
-        });
+        }
+
+        fwrite($exportFile, "-- Total: $total");
+        fclose($exportFile);
     }
 }
